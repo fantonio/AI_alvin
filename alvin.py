904124ba8e2a94dda6ffb8dc57ec4d869aa28b23
neviim <jadsneviim@gmail.com>
2012-05-02 18:51:35 -0300

    Melhoria na camada de interação do Bot, em seu visual de interação.
    Melhoramento na forma com a qual a interação é feita colocando em cores
    quando o usuário interage e outra cor quando o bot responde, retirando
    a frase "Fale algo ao bot" que toda hora aparecia, objetivo de deixar a
    interface mais harmoniosa para com a interação do usuário.

--- alvin.py
+++ alvin.py
@@ -3,13 +3,32 @@
 import aiml
 import os
 
+# defini cores texto.
+CorVerme = '\033[31m'
+CorVerde = '\033[32m' 
+CorAzul  = '\033[34m'
+CorOrig  = '\033[0;0m'
+
 #os.chdir('') # diretório que contém os arquivos da AIML standard
-ai = aiml.Kernel() # inicialização
+
+# inicialização
+ai = aiml.Kernel() 
 ai.learn('std-startup.xml') # lê o arquivo principal da AIML e faz referências aos outros
-ai.respond('load aiml b') # faz com que os outros arquivos da AIML sejam carregados
-nome = raw_input('Qual o seu nome:')
-print "Seja bem vindo", nome
+ai.respond('load aiml b') 	# faz com que os outros arquivos da AIML sejam carregados
+
+print ""
+nome = raw_input('Qual o seu nome: ')
+
+print "Seja bem vindo! ", nome
+print ""
+print "A partir de agora você podera conversar com o bot '<' aguarda sua interação."
+print ""
+ 
 while (1==1):
 	
-	frase = raw_input('Fale algo ao bot: ')
-	print "Resposta do bot: %s" % ai.respond(frase)	
+	#frase = raw_input('Fale algo ao bot: ')
+	frase = raw_input("bot:"+CorVerde+"< "+CorOrig) # cor vermelha na escrita.
+	
+	#print "Resposta do bot: %s" % ai.respond(frase)	
+	print "bot:"+CorAzul+">"+CorOrig+" %s" % ai.respond(frase) # cor verde escrita.
+        print ""
